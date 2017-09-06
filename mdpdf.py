96093e55ee4170a83aef709204e4dd3335ad7381
Simon Solnes <simon@solnes.co>
2017-09-07 01:45:10 +0200

    restructured code

--- mdpdf.py
+++ mdpdf.py
@@ -5,25 +5,32 @@ import re
 
 '''
 To do:
-- commandline
-    - flags
 - spans
     - citations
     - links
+    - escape characters in latex
+        - https://stackoverflow.com/questions/13655392/python-insert-a-character-into-a-string#13655397
+        - '&%$#_{}~^\\'
 - blocks
     - quote
+        - nested quotes
     - image
     - lists
+        - latex description lists
     - code
+    - tables
+        - text aligning
 - bibliography
+- title formatting
 '''
 
 def conv_span(text):
+    if len(text) < 1:
+        return ''
     def split_text(text, beg, end):
         retval = [item.split(beg) for item in text.split(end)]
         retval =  [word for sublist in retval for word in sublist]
         return retval.insert(0, '') if text[:2] is beg else retval
-
     def conv_format(text, find, beg = '', end = ''):
         retval = []
         for idx, item in enumerate(text.split(find)):
@@ -44,32 +51,54 @@ def conv_span(text):
         else:
             retval.append(part)
 
+    # escape chars here
     return ''.join(retval)
 
-def conv_title(text):
-    level = len(re.search('#*', text).group())
-    subs = ''.join(['sub' for i in range(level - 1)])
-    return '\\' + subs + 'section{' + conv_span(text[level + 1:]) + '}'
+def conv_list(lines):
+    pass
+def conv_quote(lines):
+    pass
+def conv_table(lines):
+    pass
+def conv_image(lines):
+    pass
+def conv_code(lines):
+    pass
 
 def conv_document(path):
+
+    def locate_end(contents, matchfunc, default):
+        for idx, line in enumerate(contents):
+            if matchfunc(line):
+                return idx + default + 1
+        return default
     tex = []
-    with open(path, 'r') as md_doc:
-        skip = False
-        for line in md_doc:
-            text = line.rstrip('\n')
-            if text == '``' and skip:
-                skip = not skip
-                continue
-            elif skip:
-                tex.append(text)
-            elif text == '``latex':
-                skip = True
-                continue
-            elif len(text) > 0:
-                if text[0] is '#':
-                    tex.append(conv_title(text))
-                else:
-                    tex.append(conv_span(text))
+    md_doc = open(path, 'r')
+    lines = [item.rstrip('\n') for item in md_doc.readlines()]
+    i = 0
+    while i < len(lines):
+        text = lines[i]
+        end = i + 1
+        # title
+        if re.match('#+\s', text):
+            level = len(re.match('#*', text).group())
+            subs = ''.join(['sub' for i in range(level - 1)])
+            block =  '\\' + subs + 'section{' + conv_span(text[level + 1:]) + '}'
+        # latex
+        elif re.match('``latex$', text):
+            end = locate_end(lines[i + 1:], lambda x: x == '``', i)
+            block = lines[i + 1: end]
+        # list
+        elif re.match('(\d\.|-|\*)\s', text):
+            end = locate_end(lines[i + 1:], lambda x: x == '', i)
+            block = conv_list(lines[i:end])
+        else:
+            block = conv_span(text)
+
+        if isinstance(block, str): tex.append(block)
+        if isinstance(block, list): tex += block
+        i = end
+    md_doc.close()
 
     return tex
 
@@ -78,25 +107,43 @@ def uses_bibliography(md_path):
 def gen_ref(md_path):
     pass
 
-if __name__ == '__main__':
+def clear_files(output_name = '*'):
+    ext = '.aux .log .toc .out .bbl .bcf .blg .dvi .bib .run.xml -blx.bib .bib .latex'
+    for i in ext.split(' '):
+        os.system('rm -f ' + output_name + i)
 
-    if len(sys.argv) != 3 or sys.argv[1] in ['-h', '-H', 'help']:
-        print('usage:\n./mdpdf.py [input md] [header file]')
-        exit()
+def parse_args(args):
+    def print_usage():
+        print('usage: mdpdf [markdown document] [header file]')
+        print('optional flags: h: help, d: debug mode, c: rm latex files')
+        exit(0)
+    debug = False
+    if len(args) == 3:
+        md_path = args[1]
+        header_path = args[2]
+    elif len(args) == 4:
+        if args[1] in ['-h', '-H', '--help', 'help']:
+            print_usage()
+        elif args[1] == '-d':
+            debug = True
+        md_path = args[2]
+        header_path = args[3]
+    elif len(args) == 2 and args[1] == '-c':
+        clear_files()
+        exit(0)
+    else:
+        print_usage()
 
-    md_path = sys.argv[1]
-    header_path = sys.argv[2]
-    output_name = sys.argv[1].split('.')[0]
-
-    debug_mode = False
-
-    # User errors
     if not os.path.isfile(md_path):
-        print('markdown document doesn\'t exist')
-        exit()
+        print('Markdown document doesn\'t exist')
+        exit(0)
     if not os.path.isfile(header_path):
-        print('header document doesn\'t exist')
-        exit()
+        print('Header document doesn\'t exist')
+        exit(0)
+
+    return debug, md_path, header_path
+
+def main(md_path, header_path, output_name):
     uses_bib = uses_bibliography(md_path)
 
     with open(output_name + '.latex', 'w') as tex_doc, open(header_path, 'r') as header:
@@ -119,11 +166,13 @@ if __name__ == '__main__':
         os.system('latex ' + output_name + '.latex')
 
     os.system('pdflatex -interaction=batchmode ' + output_name + '.latex')
-    if debug_mode:
-        exit(0)
+    
 
-    for i in ['.aux', '.log', '.toc', '.out', '.bbl', '.bcf', '.blg', '.dvi', '.run.xml', '-blx.bib', '.bib']:
-        try:
-            os.system('rmtrash ' + output_name + i + ' >> /dev/null')
-        except:
-            os.system('rm -f ' + output_name + i)
+if __name__ == '__main__':
+    debug, md_path, header_path = parse_args(sys.argv)
+    output_name = md_path.split('.')[0]
+    if debug:
+        os.system('rm -f ' + output_name + '.pdf')
+    main(md_path, header_path, output_name)
+    if not debug:
+        clear_files(output_name)
